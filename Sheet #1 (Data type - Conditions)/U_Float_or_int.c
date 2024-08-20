b79f8d0f5ca5a520df799688819e148e04884597
Alaa El Sawy <alaaelsawy927@gmail.com>
2024-08-20 17:04:27 +0300

    Update U_Float_or_int.c

--- Sheet #1 (Data type - Conditions)/U_Float_or_int.c
+++ Sheet #1 (Data type - Conditions)/U_Float_or_int.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
- 
+
 int main() {
-signed long long num1, num2;
-char c;
- 
-scanf("%lld %c %lld",&num1, &c ,&num2);
-if (c == '>'){
-if (num1 > num2){
-printf("Right");}
-else printf("Wrong");}
- 
- if (c == '<'){
- if (num1 < num2){
-printf("Right");}
-else printf("Wrong");}
- 
-if (c == '='){
-if (num1 == num2){
-printf("Right");}
-else printf("Wrong");}
- 
+    float num;
+
+    scanf("%f", &num);
+
+    int integerPart = (int)num;     /*Converts the floating-point number num to an integer and stores it in integerPart.
+                                        The (int) cast explicitly converts num to an integer, discarding the decimal part.*/
+
+    float decimalPart = num - integerPart;      // Calculates the decimal part of the number by subtracting the integer part from the original number
+
+    if (decimalPart == 0.0) {
+        printf("int %d", integerPart);
+    } else {
+        printf("float %d %.6f", integerPart, decimalPart);
+    }
+
     return 0;
 }
