df0be7f7420ac760261282f680bed6fb63ef166c
Alaa El Sawy <alaaelsawy927@gmail.com>
2024-09-07 21:07:16 +0300

    Update P_Shape1.c

--- Sheet #2 (Loops)/P_Shape1.c
+++ Sheet #2 (Loops)/P_Shape1.c
@@ -6,8 +6,8 @@ int main() {
     scanf("%d", &rows);
  
     for (int i = rows; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
-            printf("*");
+    for (int j = 1; j <= i; j++) {
+        printf("*");
         }
         printf("\n");
     }
